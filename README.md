a7183899072fca62ff5ca8a58c62abd04f304bb6
Dmitry Chepurovskiy <me@dm3ch.net>
2017-05-12 00:56:21 +0300

    Added always return zero code functionality Added Sentry exceptions reporting functionality

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # Certbot runner
 ### Script for issue and renew certs specified in YAML config using certbot.
+This project also could use Sentry for logging
 
 This script was created for `python3` and it's require `pyyaml` library
 Config example is in config_example.yaml
--- certbot-runner.py
+++ certbot-runner.py
@@ -1,7 +1,16 @@
 import sys
+import traceback
 import hashlib
 import yaml
+import logging
 from subprocess import check_call, call
+
+from raven import Client
+from raven.handlers.logging import SentryHandler
+from raven.conf import setup_logging
+
+# Specify default certbot command
+certbotBinPath = "certbot"
 
 def getConfigFileHash(file):
     return hashlib.sha256(file.read()).hexdigest()
@@ -22,6 +31,35 @@ def certbotIssueCert(certName, domainNames, dryRun):
     command.append(certName)
     check_call(command)
 
+
+def certbotWork():
+    """Issue or renew certs"""
+    if configHash != oldConfigHash:
+        check_call(["/bin/sh", "-c", "rm -rf /etc/letsencrypt/*"])
+        check_call(["/bin/sh", "-c", "rm -rf " + certsDir + "/*"])
+
+        print("\nSetting account:\n")
+        certbotAccountSet(email)
+
+        print("\nIssuing certs:\n")
+        for cert in certs:
+            certbotIssueCert(cert['certName'], cert['domainNames'], dryRun)
+
+        if not dryRun:
+            print("\nCopying certs to destination dir:\n")
+            check_call(["/bin/sh", "-c", "cp -r /etc/letsencrypt/live/* " + certsDir])
+
+            oldConfigHashFile = open(certsDir + '/config.hash', 'w')
+            oldConfigHashFile.write(configHash)
+            oldConfigHashFile.close()
+    else:
+        print("\nSetting account:\n")
+        certbotAccountSet(email)
+        print("\nRenewing certs:\n")
+        check_call([certbotBinPath, "renew", "-n", "--standalone", "--preferred-challenges", "http"])
+        print("\nCopying certs to destination dir:\n")
+        check_call(["/bin/sh", "-c", "cp -rf /etc/letsencrypt/live/* " + certsDir])
+
 if len(sys.argv) < 2:
     print("Specify config file")
     sys.exit(-1)
@@ -29,18 +67,13 @@ if len(sys.argv) < 2:
 configStream = open(sys.argv[1], 'rb')
 configHash = getConfigFileHash(configStream)
 print("Config file hash:", configHash)
-
 configStream.close()
 
 configStream = open(sys.argv[1], 'r')
 config = yaml.load(configStream)
-certbotBinPath = config['certbotBinPath']
 certsDir = config['certsDir']
-email = config['email']
-dryRun = bool(config['dryRun'])
-certs = config['certs']
-configStream.close()
 
+# Old config check start
 oldConfigHash = ''
 try:
     oldConfigHashFile = open(certsDir + '/config.hash', 'r')
@@ -49,29 +82,43 @@ try:
     oldConfigHashFile.close()
 except FileNotFoundError:
     print("Old config file doesn't exists")
+print()
+# Old config check ends
 
-if configHash != oldConfigHash:
-    check_call(["/bin/sh", "-c", "rm -rf /etc/letsencrypt/*"])
-    check_call(["/bin/sh", "-c", "rm -rf " + certsDir + "/*"])
+certbotBinPath = config['certbotBinPath']
+email = config['email']
+dryRun = bool(config['dryRun'])
+certs = config['certs']
+alwaysZeroReturnCode = bool(config["alwaysZeroReturnCode"])
+if alwaysZeroReturnCode:
+    print("Always zero return code mode is enabled")
+sentryEnable = bool(config["sentryEnable"])
+if sentryEnable:
+    sentryDSN = config["sentryDSN"]
+    print("Sentry reporting enabled, sentry DSN was found in config")
+    sentryClient = Client(sentryDSN)
+    # sentryHandler = SentryHandler(sentryClient)
+    # setup_logging(SentryHandler)
+    # logger = logging.getLogger(__name__)
+    # logger.setLevel(logging.DEBUG)
 
-    print("\nSetting account:\n")
-    certbotAccountSet(email)
+configStream.close()
 
-    print("\nIssuing certs:\n")
-    for cert in certs:
-        certbotIssueCert(cert['certName'], cert['domainNames'], dryRun)
+# if sentryEnable:
+#     print("Test")
+#     logger.fatal("Certbot runner was started")
 
-    if not dryRun:
-        print("\nCopying certs to destination dir:\n")
-        check_call(["/bin/sh", "-c", "cp -r /etc/letsencrypt/live/* " + certsDir])
+try:
+    certbotWork()
+except KeyboardInterrupt:
+    pass
+except Exception:
+    if sentryEnable:
+        sentryClient.captureException()
 
-        oldConfigHashFile = open(certsDir+'/config.hash', 'w')
-        oldConfigHashFile.write(configHash)
-        oldConfigHashFile.close()
-else:
-    print("\nSetting account:\n")
-    certbotAccountSet(email)
-    print("\nRenewing certs:\n")
-    check_call([certbotBinPath, "renew", "-n", "--standalone", "--preferred-challenges", "http"])
-    print("\nCopying certs to destination dir:\n")
-    check_call(["/bin/sh", "-c", "cp -rf /etc/letsencrypt/live/* " + certsDir])
+    if alwaysZeroReturnCode:
+        traceback.print_exc()
+        print("\n Real exit status: 0")
+        sys.exit()
+    else:
+        raise
